6e29bbb74a36c1e55648957750ba0cb2595818d7
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-04-10 03:37:33 +0900

    return_stories

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,11 +1,9 @@
 // import React, { , useTransition } from 'react';
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import instance from './../axios'
-import { requests } from './../request'
 
-interface Props {
-  fetchUrl: string
-}
+// interface Props {
+// }
 
 interface Story {
   id: number
@@ -19,16 +17,17 @@ interface Story {
 export const fetchTopStories = async (count: number): Promise<Story[]> => {
   const ids = await instance
     .get(`/topstories.json?print=pretty`)
-    .then((res) => res.json())
+    .then((res: { json: () => any }) => res.json())
   const stories: any = await Promise.all(
     ids.slice(0, count).map(
       async (id: number): Promise<Story> => {
         const story: Story = await instance
           .get(`/${id}.json?print=pretty`)
-          .then((res) => res.json())
+          .then((res: { json: () => any }) => res.json())
         return story
       },
     ),
   )
   console.group(stories)
+  return stories
 }
